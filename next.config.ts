2695f589a354e6551a1ae4fcc09c44f56d3aca13
NeeteshCTGS <neetesh@ctgs.in>
2026-09-17 11:49:40 +0530

    Fixing

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,12 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'app.gaubook.org' },
       { protocol: 'https', hostname: '*.gaubook.org' },
+      // Post / profile / product images are served from S3
+      // (https://<bucket>.s3.<region>.amazonaws.com/...)
+      { protocol: 'https', hostname: '*.s3.ap-south-1.amazonaws.com' },
+      // Google Places photos surfaced in explore / gaushala listings
+      { protocol: 'https', hostname: 'maps.googleapis.com' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
     ],
   },
   async headers() {
